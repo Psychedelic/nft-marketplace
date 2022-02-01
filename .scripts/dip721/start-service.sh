2ae2c0bd72c1125cb0cee6a17b60c533ee6bc0c2
Helder Oliveira <heldrida@gmail.com>
2022-02-01 18:44:09 +0000

    refactor: 💡 dip721 start service script

--- .scripts/dip721/start-service.sh
+++ .scripts/dip721/start-service.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-CAP_IC_HISTORY_ROUTER_ID=$(cd ./cap && dfx canister id ic-history-router)
-DIP721_TOKEN_SHORT=CRW
-DIP721_TOKEN_NAME=Crown
-
 cd $(dirname $BASH_SOURCE) || exit 1
 
 cd ../../DIP721 || exit 1
+
+CAP_IC_HISTORY_ROUTER_ID=$(cd ../../cap && dfx canister id ic-history-router)
+
+DIP721_TOKEN_SHORT=CRW
+DIP721_TOKEN_NAME=Crown
 
 dfx deploy --no-wallet nft --argument "(principal \"$(dfx identity get-principal)\", \"$DIP721_TOKEN_SHORT\", \"$DIP721_TOKEN_NAME\", principal \"$CAP_IC_HISTORY_ROUTER_ID\")"
