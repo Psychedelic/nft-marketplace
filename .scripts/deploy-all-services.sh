3188da8558f4c80ee8e97acbc7de3041c918be2b
Helder Oliveira <heldrida@gmail.com>
2022-05-27 18:06:17 +0100

    fix: 🐛 on mocking, topup system user instead of allowance and approval for wicp #82

--- .scripts/deploy-all-services.sh
+++ .scripts/deploy-all-services.sh
@@ -66,21 +66,14 @@ deployWICP() {
   # a2t6b-nznbt-igjd3-ut25i-b43cf-mt45v-g3x2g-ro6h5-kowno-dx3rz-uqe
   _mockSystemIdentity="a2t6b-nznbt-igjd3-ut25i-b43cf-mt45v-g3x2g-ro6h5-kowno-dx3rz-uqe"
 
-  printf "🤖 wICP approve and set allowance for mock system identity %s\n" "$_mockSystemIdentity"
+  printf "🤖 wICP topup for mock system identity %s\n" "$_mockSystemIdentity"
 
   dfx canister \
     call --update "$_wicpId" \
-    approve "( 
+    transfer "( 
       principal \"$_mockSystemIdentity\",
-      9_000_000_000_000:nat
-    )"
-
-  dfx canister \
-    call --update "$_wicpId" \
-    allowance "( 
-      principal \"$_wallet\",
-      principal \"$_mockSystemIdentity\",
-    )"
+      9_000_000_000_000:nat,
+    )"  
 }
 
 deployCapRouter
